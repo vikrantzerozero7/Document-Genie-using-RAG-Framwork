775223254ecfe3d82409827701b7091ab2d765e7
vikrantzerozero8 <116074891+vikrantzerozero8@users.noreply.github.com>
2024-07-16 21:54:21 +0530

    Update streamlit_App2.py

--- streamlit_App2.py
+++ streamlit_App2.py
@@ -28,7 +28,7 @@ def get_text_chunks(text):
     return chunks
 
 def get_vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001",,google_api_key=api_key)
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key=api_key)
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
 
